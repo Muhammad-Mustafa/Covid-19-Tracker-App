648ecb449cb6c3db0e768d93c1e35fc003f0f011
Muhammad-Mustafa <mustafaraziq30@gmail.com>
2020-12-24 03:53:24 +0500

    Test

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import {Link, Typography} from '@material-ui/core';
-import styles from './Github.module.css'
+import React from "react";
+import { Link, Typography } from "@material-ui/core";
+import styles from "./Github.module.css";
 
-const Github = () =>{
-    return(
-        <div className={styles.text}>
-            <Link href="https://github.com/Muhammad-Mustafa/Covid-19-Tracker-App" underline='hover'  rel="noreferrer" target='_blank'>
-            <Typography variant="subtitle2" align="justify" color="textPrimary" styles={styles.typo}>
-                {/* <Link componen="button" > */}
-                Click Here For the Github Repository Don't Forget to give me a Star 🌟
-                {/* </Link> */}
-            </Typography>
-            </Link>
-        </div>
-    )
-}
+const Github = () => {
+  return (
+    <div className={styles.text}>
+      <Link
+        href="https://github.com/Muhammad-Mustafa/Covid-19-Tracker-App"
+        underline="hover"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <Typography
+          variant="subtitle2"
+          align="justify"
+          color="textPrimary"
+          styles={styles.typo}
+        >
+          {/* <Link componen="button" > */}
+          Shujaat bhai zindabad Click Here For the Github Repository Don't
+          Forget to give me a Star 🌟
+          {/* </Link> */}
+        </Typography>
+      </Link>
+    </div>
+  );
+};
 
 export default Github;
